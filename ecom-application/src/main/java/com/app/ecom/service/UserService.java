1d9fc826e8284f947598b0a0c89f72c9f5506aff
Amos Uzuagie <amosuzuagie@gmail.com>
2025-05-16 10:49:19 +0100

    feat: add H2 database configuration

--- ecom-application/src/main/java/com/app/ecom/service/UserService.java
+++ ecom-application/src/main/java/com/app/ecom/service/UserService.java
@@ -1,41 +1,37 @@
 package com.app.ecom.service;
 
 import com.app.ecom.model.User;
+import com.app.ecom.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 public class UserService {
-    private final List<User> userList = new ArrayList<>();
-    private long idCount = 1L;
+    @Autowired
+    private UserRepository userRepository;
 
     public List<User> fetchAllUser() {
-        return userList;
+        return userRepository.findAll();
     }
 
     public void addUser(User user) {
-        user.setId(idCount++);
-        userList.add(user);
+        userRepository.save(user);
     }
 
 
     public Optional<User> fetchUser(long id) {
-        return userList.stream()
-                .filter(user -> user.getId().equals(id))
-                .findFirst();
+        return userRepository.findById(id);
     }
 
     public boolean updateUser(long id, User userUpdate) {
-        return userList.stream()
-                .filter( user -> user.getId().equals(id))
-                .findFirst()
-                .map(existingUser -> {
-                    existingUser.setLastName(userUpdate.getLastName());
-                    existingUser.setFirstName(userUpdate.getFirstName());
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setLastName(userUpdate.getLastName());
+                    user.setFirstName(userUpdate.getFirstName());
+                    userRepository.save(user);
                     return true;
                 }).orElse(false);
     }
